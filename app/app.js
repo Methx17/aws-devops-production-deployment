1f028ff4fd74597fe728efc6ead0eca7d22670a7
methx17 <metheshps444@gmail.com>
2026-07-14 02:18:54 +0530

    Initial project setup

---
+++ app/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "AWS DevOps Assignment",
+        developer: "Methesh Shetty",
+        status: "Running Successfully",
+        timestamp: new Date()
+    });
+});
+
+app.get('/health', (req, res) => {
+    res.json({
+        status: "Healthy"
+    });
+});
+
+app.get('/users', (req, res) => {
+    res.json([
+        {
+            id: 1,
+            name: "John"
+        },
+        {
+            id: 2,
+            name: "David"
+        },
+        {
+            id: 3,
+            name: "Emma"
+        }
+    ]);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
